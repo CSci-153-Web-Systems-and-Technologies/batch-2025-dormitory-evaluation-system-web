85032143af71c8e6757bdb90c9e5d718099c5aac
Dohn Michael Varquez <23-1-02140@vsu.edu.ph>
2025-10-10 15:44:43 +0800

    feat: add Tabler icons and enhance dashboard layout with new card components

--- dormitory-evaluation-system/src/app/(protected)/layout.tsx
+++ dormitory-evaluation-system/src/app/(protected)/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import { Toaster } from "sonner"
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { SidebarProvider, SidebarTrigger, SidebarInset } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
 
 const geistSans = Geist({
@@ -30,14 +30,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
     >
-          <SidebarProvider>
-      <AppSidebar />
-      <main>
-        <SidebarTrigger />
-        <Toaster />
-        {children}
-      </main>
-    </SidebarProvider>
+              <SidebarProvider>
+                <AppSidebar />
+                <SidebarInset>
+                  <SidebarTrigger />
+                  <Toaster />
+                  {children}
+                </SidebarInset>
+              </SidebarProvider>
       </body>
     </html>
   );
